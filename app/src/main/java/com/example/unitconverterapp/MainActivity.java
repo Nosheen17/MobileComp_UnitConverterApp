4600656ac1ad15e7c97ab420038d306b9d479917
Nosheen17 <hayat.mehreen17@gmail.com>
2022-04-30 19:58:43 +0500

    Declaring Widgets

--- app/src/main/java/com/example/unitconverterapp/MainActivity.java
+++ app/src/main/java/com/example/unitconverterapp/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.unitconverterapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText editText;
+    TextView textView,textView2,textView4,valueOfPounds;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+      
+
+
     }
 }
